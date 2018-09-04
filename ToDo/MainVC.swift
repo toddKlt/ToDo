40bf98838f9ac268ad519676827346515b61886e
Theodoros Kaltikopoulos <todkalt@gmail.com>
2018-09-04 17:53:11 +0300

    Todo list complete / datasource & delegate methods

---
+++ ToDo/MainVC.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Theodoros Kaltikopoulos on 04/09/2018.
+//  Copyright © 2018 mania. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UITableViewController {
+
+    let itemArray = ["Mike", "Eggs", "Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+
+    }
+}
+
