f916fd444ed2370dd2b3a48d1fd01298ad920bd3
Theodoros Kaltikopoulos <todkalt@gmail.com>
2018-09-06 11:59:16 +0300

    Convert to NVC design pattern

--- ToDo/Controller/MainVC.swift
+++ ToDo/Controller/MainVC.swift
@@ -10,18 +10,26 @@ import UIKit
 
 class MainVC: UITableViewController {
 
-    var itemArray = Array<String>()//["Mike", "Eggs", "Demogorgon"]
+//    var itemArray = Array<String>()//["Mike", "Eggs", "Demogorgon"]
+    let testArray = ["Mike", "Eggs", "Demogorgon"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
 //    var itemArray = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        
+        for name in testArray {
+            let newItem = Item()
+            newItem.title = name
+            newItem.done = false
+            itemArray.append(newItem)
+        }
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
-//        tableView.reloadData()
     }
 
-    //MARK - TableView Datasource Methods
+    //MARK - TableView Datasource Methods 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,20 +37,19 @@ class MainVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        // set value of cell.accessoryType to .checkmark or .none depending on the value true/false of item.done
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
     }
     
     //MARK - Add New Items
@@ -51,7 +58,10 @@ class MainVC: UITableViewController {
         let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add button
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
---
+++ ToDo/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Theodoros Kaltikopoulos on 06/09/2018.
+//  Copyright © 2018 mania. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
